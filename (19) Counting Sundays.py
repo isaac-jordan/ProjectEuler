64491cf1601351a25435c08adaf4628e3bf74a58
Isaac Jordan <sheepses6@hotmail.co.uk>
2015-01-01 21:27:16 +0000

    Solved Problem 19

--- (19) Counting Sundays.py
+++ (19) Counting Sundays.py
@@ -1,21 +1,15 @@
-
+import calendar
 
 def countSundays():
     sundayCount = 0
-    
-    year = 1900
-    while year < 2001:
-        if year % 4 == 0:
-            days = 366
-            leap = True
-        else:
-            days = 365
-            leap = False
-        day = 1
-        while day < days:
-            if day % 7 == 0: #This only works for the first year...
-                if leap == True and day in [1,32,60,91,120,152,182,213,244,274,304,334]:
-                    sundayCount += 1
+    for year in range(1901,2001):
+        for month in range(1,13):
+	    if (calendar.weekday(year, month, 1) == 6):
+	        sundayCount += 1
+    return sundayCount
+
+print countSundays()
+                
                     
         
     
